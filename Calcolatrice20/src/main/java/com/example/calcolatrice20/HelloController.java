82e71f1b4f9621aea9af69fbc9202c86ad6a879a
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-05-31 19:17:51 +0200

    Calcolatrice normale senza decimali

--- Calcolatrice20/src/main/java/com/example/calcolatrice20/HelloController.java
+++ Calcolatrice20/src/main/java/com/example/calcolatrice20/HelloController.java
@@ -4,101 +4,158 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-
+    private int v1; //Numero 1
+    private String operation = ""; //Operazione da eseguire
+    private int inseriti=0;
     @FXML
     private TextField riqId;
 
     @FXML
     protected void onCasele1() {
-        riqId.setText(riqId.getText()+1);
+        riqId.appendText("1");
     }
     @FXML
     protected void onCasele2() {
-        riqId.setText(riqId.getText()+2);
+        riqId.appendText("2");
     }
+
     @FXML
     protected void onCasele3() {
-        riqId.setText(riqId.getText()+3);
+        riqId.appendText("3");
     }
+
     @FXML
     protected void onCasele4() {
-        riqId.setText(riqId.getText()+4);
+        riqId.appendText("4");
     }
+
     @FXML
     protected void onCasele5() {
-        riqId.setText(riqId.getText()+5);
+        riqId.appendText("5");
     }
+
     @FXML
     protected void onCasele6() {
-        riqId.setText(riqId.getText()+6);
+        riqId.appendText("6");
     }
+
     @FXML
     protected void onCasele7() {
-        riqId.setText(riqId.getText()+7);
+        riqId.appendText("7");
     }
+
     @FXML
     protected void onCasele8() {
-        riqId.setText(riqId.getText()+8);
+        riqId.appendText("8");
     }
+
     @FXML
     protected void onCasele9() {
-        riqId.setText(riqId.getText()+9);
+        riqId.appendText("9");
     }
+
     @FXML
     protected void onCasele0() {
-        riqId.setText(riqId.getText()+0);
+        riqId.appendText("0");
     }
+
     @FXML
     protected void onCasele00() {
-        riqId.setText(riqId.getText()+"00");
-    }
-    @FXML
-    protected void c() {
-        String s="";
-        for (int i=0;i<riqId.getText().length()-1;i++){
-            s=s+riqId.getText().charAt(i);
-        }
-        riqId.setText(s);
-
+        riqId.appendText("00");
     }
     @FXML
     protected void cAll() {
-        riqId.setText("0");
-
+        riqId.setText("");
+        v1 = 0;
+        operation = "";
+        inseriti=0;
+    }
+    @FXML
+    protected void c() {
+        String numCorrente = riqId.getText();
+        if (!numCorrente.isEmpty()) {
+            riqId.setText(numCorrente.substring(0, numCorrente.length() - 1));
+        }
     }
     @FXML
     protected void onPiu() {
-        int v1=Integer.parseInt(riqId.getText());
-        riqId.setText("0");
-        int v2=Integer.parseInt(riqId.getText());
-        riqId.setText(String.valueOf(v1+v2));
-
+        if (inseriti==0)
+            v1=Integer.parseInt(riqId.getText());
+        else
+            v1=v1+Integer.parseInt(riqId.getText());
+        riqId.setText("");
+        operation="+";
+        inseriti++;
     }
     @FXML
     protected void onMeno() {
-        int v1=Integer.parseInt(riqId.getText());
-        riqId.setText("0");
-        int v2=Integer.parseInt(riqId.getText());
-        riqId.setText(String.valueOf(v1-v2));
+        if (inseriti==0)
+            v1=Integer.parseInt(riqId.getText());
+        else
+            v1 =v1-Integer.parseInt(riqId.getText());
+        riqId.setText("");
+        operation = "-";
+        inseriti++;
     }
+
     @FXML
     protected void onPer() {
-        int v1=Integer.parseInt(riqId.getText());
-        riqId.setText("0");
-        int v2=Integer.parseInt(riqId.getText());
-        riqId.setText(String.valueOf(v1*v2));
+        if (inseriti==0)
+            v1=Integer.parseInt(riqId.getText());
+        else
+            v1 =v1*Integer.parseInt(riqId.getText());
+        riqId.setText("");
+        operation = "*";
+        inseriti++;
     }
+
     @FXML
     protected void onDiviso() {
-        int v1=Integer.parseInt(riqId.getText());
-        riqId.setText("0");
-        int v2=Integer.parseInt(riqId.getText());
-        if (v1==0 && v2==0)
-            riqId.setText("Indeterminato");
-        else if (v2==0)
-            riqId.setText("Impossibile");
-        else
-            riqId.setText(String.valueOf(v1/v2));
-
+        if (inseriti==0)
+            v1=Integer.parseInt(riqId.getText());
+        else{
+            if (v1==0 && Integer.parseInt(riqId.getText())==0) {
+                riqId.setText("Indeterminato");
+            }
+            else if (Integer.parseInt(riqId.getText()) == 0) {
+                riqId.setText("Impossibile");
+            } else {
+                v1 =v1/Integer.parseInt(riqId.getText());
+            }
+        }
+        riqId.setText("");
+        operation = "/";
+        inseriti++;
+    }
+    @FXML
+    protected void onUguale() {
+        int v2 = Integer.parseInt(riqId.getText());
+        int result;
+        switch (operation) {
+            case "+":
+                result = v1 + v2;
+                break;
+            case "-":
+                result = v1 - v2;
+                break;
+            case "*":
+                result = v1 * v2;
+                break;
+            case "/":
+                if (v1==0 && v2==0) {
+                    riqId.setText("Indeterminato");
+                    return;
+                }
+                if (v2 == 0) {
+                    riqId.setText("Impossibile");
+                    return;
+                } else {
+                    result = v1 / v2;
+                }
+                break;
+            default:
+                return;
+        }
+        riqId.setText(String.valueOf(result));
     }
 }
